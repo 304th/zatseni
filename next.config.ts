4c6c43c20b97bf24eb0fc7eaf4abb8bebbbc8c96
avkulpin <avkulpin@yandex.com>
2026-01-02 14:28:49 +0300

    fix: add non-www to www redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "otzovik.ai",
+          },
+        ],
+        destination: "https://www.otzovik.ai/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
